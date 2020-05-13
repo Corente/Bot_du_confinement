a44d4e2d7e543bfe400c92a802b74f8e5e6ea0e5
Corentin Ourvoy <corente94@hotmail.fr>
2020-05-13 19:35:47 +0200

    Version avec prise en charge de la fin du confinement

--- bot.py
+++ bot.py
@@ -2,12 +2,12 @@ import discord, os, asyncio, schedule
 from datetime import date, time, datetime
 from discord.ext import commands
 
-TOKEN = 'Le token du bot'
+TOKEN = 'YOUR TOKEN HERE'
 
 description = '''ConfinnementBot in Python'''
 bot = commands.Bot(command_prefix='?')
 
-channels = ['Les channels id ou envoyer les msg']
+channels = ['Chanels id you want to have automatic message every day']
 
 @bot.event
 async def on_ready():
@@ -20,13 +20,18 @@ async def time_check():
     await bot.wait_until_ready()
     while not bot.is_closed():
         now = datetime.strftime(datetime.now(),'%H:%M')
-        if (now == 'L heure du message journalier :  HH:MM'):
+        if (now == '12:00'):
             for i in channels:
                 sender = bot.get_channel(i)
-                fin = datetime('la date de fin: yyyy, mm, dd')
+                fin = datetime('END DATE yyyy, mm, dd')
                 mtn = datetime.now()
                 ret = fin - mtn
-                m = "Il reste " + str(ret.days) + " jours avant la fin du confinement."
+                if (ret.days > 0):
+                    m = "Il reste " + str(ret.days) + " jours avant la fin du confinement."
+                elif (ret.days == 0):
+                    m = "C'est le dernier jour du confinement." 
+                else:
+                    m = "Le confinement est fini !"
                 await sender.send(m)
             t = 90
         else:
@@ -36,11 +41,21 @@ async def time_check():
 @bot.command()
 async def timer(ctx):
     """Affiche le temps restant en confinnement"""
-    fin = datetime('la date de fin: yyyy, mm, dd')
+    fin = datetime('END DATE yyyy, mm, dd')
     mtn = datetime.now()
     ret = fin - mtn
-    send = "Il reste " + str(ret.days) + " jours avant la fin du confinement."
-    await ctx.send(send)
+    if (ret.days > 0):
+        m = "Il reste " + str(ret.days) + " jours avant la fin du confinement."
+    elif (ret.days == 0):
+        m = "C'est le dernier jour du confinement." 
+    else:
+        m = "Le confinement est fini !"    
+    await ctx.send(m)
+
+@bot.command()
+async def que_faire(ctx):
+    """Vous indique la procdure à suivre"""
+    await ctx.send("Restez chez vous !")
 
 bot.loop.create_task(time_check())
 bot.run(TOKEN)
